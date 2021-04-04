f299aa3890f014f597411ac1309a483233bbca78
Pablo Giraud-Carrier <pablo.giraud-carrier@epitech.eu>
2021-04-04 16:15:19 +0200

    fix: stage2.sys file name

--- src/stage2.asm
+++ src/stage2.asm
@@ -8,13 +8,13 @@
 ; We do not have the limitation of 512 bytes here,
 ; so we can add anything we want here!
  
-org 0x0		; offset to 0, we will set segments later
+org 0x0								; offset to 0, we will set segments later
  
-bits 16		; we are still in real mode
+bits 16								; we are still in real mode
  
 ; we are loaded at linear address 0x10000
  
-jmp main	; jump to main
+jmp main							; jump to main
  
 ;*************************************************;
 ;	Prints a string
@@ -22,32 +22,32 @@ jmp main	; jump to main
 ;************************************************;
  
 Print:
-			lodsb		; load next byte from string from SI to AL
-			or	al, al	; Does AL=0?
-			jz	PrintDone	; Yep, null terminator found-bail out
-			mov	ah,	0eh	; Nope-Print the character
-			int	10h
-			jmp	Print	; Repeat until null terminator found
+	lodsb								; load next byte from string from SI to AL
+	or				al, al		; Does AL=0?
+	jz				PrintDone	; Yep, null terminator found-bail out
+	mov				ah,	0eh		; Nope-Print the character
+	int				10h
+	jmp				Print			; Repeat until null terminator found
 PrintDone:
-			ret		; we are done, so return
- 
+	ret									; we are done, so return
+
 ;*************************************************;
 ;	Second Stage Loader Entry Point
 ;************************************************;
- 
+
 main:
-			cli		; clear interrupts
-			push	cs	; Insure DS=CS
-			pop	ds
- 
-			mov	si, Msg
-			call	Print
- 
-			cli		; clear interrupts to prevent triple faults
-			hlt		; hault the system
- 
+	cli									; clear interrupts
+	push			cs				; Insure DS=CS
+	pop				ds
+
+	mov				si, msgLoading
+	call			Print
+
+	cli									; clear interrupts to prevent triple faults
+	hlt									; hault the system
+
 ;*************************************************;
 ;	Data Section
 ;************************************************;
- 
-Msg	db	"Preparing to load operating system...",13,10,0
+
+msgLoading	db	"Preparing to devOS...",13,10,0
